f2f5fde62a0ec75cf98ed2f44e760cb888017467
lniiwuw <lniiwuw@gmail.com>
2023-11-10 10:30:08 +0800

    change the construct method to be public

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,23 +1,24 @@
-template <typename T = int, int inf = 0x3f3f3f3f>
+template <typename T>
 class FenwickTree {
 private:
     int n;
-    vector<T> v;
-    FenwickTree(int n) : n(n), v(n, inf) {}
+    T inf;
+    std::vector<T> v;
 public:
+    FenwickTree(int n, T inf) : n(n), inf(inf), v(n, inf) {}
     void update(int x, T y) {
         x += 1;
         for (; x <= n; x += x & -x) {
-            v[x - 1] = min(v[x - 1], y);
+            v[x - 1] = std::min(v[x - 1], y);
         }
     }
     
     T query(int x) {
-        x = min(x, n - 1);
+        x = std::min(x, n - 1);
         T y = inf;
         x += 1;
         for (; x; x -= x & -x) {
-            y = min(y, v[x - 1]);
+            y = std::min(y, v[x - 1]);
         }
         return y;
     }
