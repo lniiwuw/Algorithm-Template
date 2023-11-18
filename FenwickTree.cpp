781f479b5659a0e3a3cb72a85dbd6dcc1884222d
lniiwuw <lniiwuw@gmail.com>
2023-11-18 17:23:05 +0800

    add fenwickTree for sum

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,3 +1,4 @@
+/*求[l, r]的最小值*/
 template <typename T>
 class FenwickTree {
 private:
@@ -21,5 +22,31 @@ public:
             y = std::min(y, v[x - 1]);
         }
         return y;
+    }
+};
+
+/*求[l, r]的和*/
+template <typename T>
+class FenwickTree {
+private:
+    int n;
+    std::vector<T> v;
+public:
+    FenwickTree(int n) : n(n), v(n) {}
+    void update(int x, T y) {
+        x += 1;
+        for (; x <= n; x += x & -x) {
+            v[x - 1] += y;
+        }
+    }
+    
+    T query(int x) {
+        x = std::min(x, n - 1);
+        T sum = 0;
+        x += 1;
+        for (; x > 0; x -= x & -x) {
+            sum += = v[x - 1];
+        }
+        return sum;
     }
 };
