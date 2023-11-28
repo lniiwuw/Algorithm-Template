011557ee3305db100fa03f169daeb09fd303b5af
lniiwuw <lniiwuw@gmail.com>
2023-11-28 08:49:46 +0800

    add strongly connected component

---
+++ SCC.cpp
@@ -0,0 +1,60 @@
+// strongly connected component
+struct SCC {
+    int n;
+    std::vector<std::vector<int>> adj;
+    std::vector<int> stack;
+    std::vector<int> dfn, low, belong;
+    int cur, cnt;
+    
+    SCC() {}
+    SCC(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        this->n = n;
+        adj.assign(n, {});
+        dfn.assign(n, -1);
+        low.resize(n);
+        belong.assign(n, -1);
+        stack.clear();
+        cur = cnt = 0;
+    }
+    
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+    }
+    
+    void dfs(int x) {
+        dfn[x] = low[x] = cur++;
+        stack.push_back(x);
+        
+        for (auto y : adj[x]) {
+            if (dfn[y] == -1) {
+                dfs(y);
+                low[x] = std::min(low[x], low[y]);
+            } else if (belong[y] == -1) {
+                low[x] = std::min(low[x], dfn[y]);
+            }
+        }
+        
+        if (dfn[x] == low[x]) {
+            int y;
+            do {
+                y = stack.back();
+                belong[y] = cnt;
+                stack.pop_back();
+            } while (y != x);
+            cnt++;
+        }
+    }
+    
+    std::vector<int> work() {
+        for (int i = 0; i < n; i++) {
+            if (dfn[i] == -1) {
+                dfs(i);
+            }
+        }
+        return belong;
+    }
+};
