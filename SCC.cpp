e0430ae0f12cc6d6f6cbdb911c8cb531742524f6
lniiwuw <lniiwuw@gmail.com>
2023-11-30 19:19:56 +0800

    add kosaraju algorithm

--- SCC.cpp
+++ SCC.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 // strongly connected component
+// tarjan algorithm  O(n + m)
 struct SCC {
     int n;
     std::vector<std::vector<int>> adj;
@@ -55,6 +56,61 @@ struct SCC {
         for (int i = 0; i < n; i++) {
             if (dfn[i] == -1) {
                 dfs(i);
+            }
+        }
+        return belong;
+    }
+};
+
+// kosaraju algothrim O(n + m)
+struct kosaraju {
+    int n, sccCnt;
+    std::vector<std::vector<int>> e, re;
+    std::vector<bool> vis;
+    std::vector<int> belong, stack;
+    
+    kosaraju() {}
+    kosaraju(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        this->n = n;
+        this->sccCnt = 0;
+        e.assign(n, {});
+        re.assign(n, {});
+        vis.resize(n);
+        belong.assign(n, -1);
+    }
+    
+    void addEdge(int u, int v) {
+        e[u].push_back(v);
+        re[v].push_back(u);
+    }
+
+    void dfs1(int u) {
+        vis[u] = true;
+        for (int v : e[u])
+            if (!vis[v])
+                dfs1(v);
+        stack.push_back(u);
+    }
+
+    void dfs2(int u) {
+        belong[u] = sccCnt;
+        for (int v : re[u])
+            if (belong[v] == -1)
+                dfs2(v);
+    }
+
+    std::vector<int> work() {
+        for (int i = 0; i < n; ++i)
+            if (!vis[i])
+                dfs1(i);
+        for (int i = n - 1; i >= 0; --i) {
+            if (belong[stack[i]] == -1) {
+                dfs2(stack[i]);
+                sccCnt ++;
             }
         }
         return belong;
