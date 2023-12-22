f3bbb63035c91cb29e7273af5c5583d9b899517e
lniiwuw <lniiwuw@gmail.com>
2023-12-22 18:26:24 +0800

    change "struct" to "class template"

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -1,11 +1,12 @@
-#include <bits/stdc++.h>
-
-struct SparseTable{
+template <typename T>
+class SparseTable{
+private:
     int n;
-    std::vector<std::vector<int>> dpmx, dpmn;
-    std::vector<int> lg, v;
+    std::vector<std::vector<T>> dpmx, dpmn;
+    std::vector<int> lg;
 
-    SparseTable (std::vector<int> &a) : v(a){
+public:
+    SparseTable (std::vector<T> &a) {
         n = a.size();
         lg.resize(n + 2);
         for (int i = 2; i <= n; i++) {
@@ -13,10 +14,10 @@ struct SparseTable{
         }
 
         int m = 33 - __builtin_clz(n);
-        dpmx.resize(n, std::vector<int> (m));
-        dpmn.resize(n, std::vector<int> (m));
+        dpmx.resize(n, std::vector<T> (m));
+        dpmn.resize(n, std::vector<T> (m));
         for (int i = 0; i < n; i++) {
-            dpmx[i][0] = dpmn[i][0] = v[i];
+            dpmx[i][0] = dpmn[i][0] = a[i];
         }
         for (int j = 1; j < m; j++) {
             for (int i = 0; i + (1 << j) - 1 < n; i++) {
@@ -26,12 +27,12 @@ struct SparseTable{
         }
     }
 
-    int getmax(int l, int r) {
+    T getmax(int l, int r) {
         int k = lg[r - l + 1];
         return std::max(dpmx[l][k], dpmx[r - (1 << k) + 1][k]);
     }
 
-    int getmin(int l, int r) {
+    T getmin(int l, int r) {
         int k = lg[r - l + 1];
         return std::min(dpmn[l][k], dpmn[r - (1 << k) + 1][k]);
     }
