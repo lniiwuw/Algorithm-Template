38aa408b6698dc0099f92870c50701fe9c3886b6
lniiwuw <lniiwuw@gmail.com>
2024-01-03 10:04:46 +0800

    add CombinationNumber class

--- CombinationNumber.cpp
+++ CombinationNumber.cpp
@@ -1,7 +1,8 @@
-const int MOD = 1'000'000'007;
-const int MX = 100'000;
+#include <bits/stdc++.h>
+// const int MOD = 1'000'000'007;
+const int MOD = 998'244'353;
 
-long long q_pow(long long x, int n) {
+long long q_pow(long long x, long long n) {
     long long res = 1;
     for (; n > 0; n /= 2) {
         if (n % 2) {
@@ -12,21 +13,49 @@ long long q_pow(long long x, int n) {
     return res;
 }
 
-// combination number template
-long long fac[MX], inv_fac[MX];
-
-auto init = [] {
-    fac[0] = 1;
-    for (int i = 1; i < MX; i++) {
-        fac[i] = fac[i - 1] * i % MOD;
+class Comb {
+private:
+    int n, MOD;
+    std::vector<long long> _fac;
+    std::vector<long long> _invfac;
+    std::vector<long long> _inv;
+public:
+    Comb(int MOD) : n{0}, _fac{1}, _invfac{1}, _inv{0}, MOD(MOD) {}
+    Comb(int MOD, int n) : Comb(MOD) {
+        init(n);
     }
-    inv_fac[MX - 1] = q_pow(fac[MX - 1], MOD - 2);
-    for (int i = MX - 1; i > 0; i--) {
-        inv_fac[i - 1] = inv_fac[i] * i % MOD;
+    
+    void init(int m) {
+        if (m <= n) return;
+        _fac.resize(m + 1);
+        _invfac.resize(m + 1);
+        _inv.resize(m + 1);
+        
+        for (int i = n + 1; i <= m; i++) {
+            _fac[i] = _fac[i - 1] * i % MOD;
+        }
+        _invfac[m] = q_pow(_fac[m], MOD - 2);
+        for (int i = m; i > n; i--) {
+            _invfac[i - 1] = _invfac[i] * i % MOD;
+            _inv[i] = _invfac[i] * _fac[i - 1] % MOD;
+        }
+        n = m;
     }
-    return 0;
-}();
-
-long long comb(int n, int k) {
-    return fac[n] * inv_fac[k] % MOD * inv_fac[n - k] % MOD;
-}
+    
+    long long fac(int m) {
+        if (m > n) init(2 * m);
+        return _fac[m];
+    }
+    long long invfac(int m) {
+        if (m > n) init(2 * m);
+        return _invfac[m];
+    }
+    long long inv(int m) {
+        if (m > n) init(2 * m);
+        return _inv[m];
+    }
+    long long C(int n, int m) {
+        if (n < m || m < 0) return 0;
+        return fac(n) * invfac(m) % MOD * invfac(n - m) % MOD;
+    }
+};
