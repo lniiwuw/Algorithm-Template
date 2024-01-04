7b1cf92fb99faba44217a6aa4a4f4377f384ea64
lniiwuw <lniiwuw@gmail.com>
2024-01-04 15:44:03 +0800

    Discrete method

---
+++ Discrete.cpp
@@ -0,0 +1,26 @@
+template<typename T>
+class Discrete {
+private:
+	std::vector<T> vec;
+public:
+	Discrete() {}
+	Discrete(std::vector<T> &v) : vec(v) {}
+	void add(T x) {
+		vec.push_back(x);
+	}
+	void work() {
+		sort(vec.begin(), vec.end());
+		typename std::vector<T>::iterator it = std::unique(vec.begin(), vec.end());
+		if (it != vec.end()) vec.erase(it, vec.end());
+	}
+	int getId(T x) {
+		return std::lower_bound(vec.begin(), vec.end(), x) - vec.begin();
+	}
+	int size() {
+		return vec.size();
+	}
+	T operator[] (int id) {
+		assert(id < vec.size());
+		return vec[id];
+	}
+};
