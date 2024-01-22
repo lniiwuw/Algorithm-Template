b96618339cbf15ea87774762b79b839ac464913a
lniiwuw <lniiwuw@gmail.com>
2024-01-22 16:56:04 +0800

    add query maximum value in range [0, r]

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,4 +1,4 @@
-/*求[l, r]的最小值*/
+/*求[0, r]的最小值*/
 template <typename T>
 class FenwickTree {
 private:
@@ -20,6 +20,33 @@ public:
         x += 1;
         for (; x > 0; x -= x & -x) {
             y = std::min(y, v[x - 1]);
+        }
+        return y;
+    }
+};
+
+/*求[0, r]的最大值*/
+template <typename T>
+class FenwickTree {
+private:
+    int n;
+    T inf;
+    std::vector<T> v;
+public:
+    FenwickTree(int n, T inf) : n(n), inf(inf), v(n, inf) {}
+    void update(int x, T y) {
+        x += 1;
+        for (; x <= n; x += x & -x) {
+            v[x - 1] = std::max(v[x - 1], y);
+        }
+    }
+    
+    T query(int x) {
+        x = std::min(x, n - 1);
+        T y = inf;
+        x += 1;
+        for (; x > 0; x -= x & -x) {
+            y = std::max(y, v[x - 1]);
         }
         return y;
     }
