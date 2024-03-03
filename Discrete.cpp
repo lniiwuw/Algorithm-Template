7a11898a66970b1ea74d66d65fe4dfdd34a87136
lniiwuw <lniiwuw@gmail.com>
2024-03-03 18:52:55 +0800

    add the mothod for insert vector to vec

--- Discrete.cpp
+++ Discrete.cpp
@@ -5,9 +5,8 @@ private:
 public:
 	Discrete() {}
 	Discrete(std::vector<T> &v) : vec(v) {}
-	void add(T x) {
-		vec.push_back(x);
-	}
+	void add(T x) { vec.push_back(x); }
+    void add(std::vector<T> v) { vec.insert(vec.end(), v.begin(), v.end()); }
 	void work() {
 		sort(vec.begin(), vec.end());
 		typename std::vector<T>::iterator it = std::unique(vec.begin(), vec.end());
@@ -16,9 +15,7 @@ public:
 	int getId(T x) {
 		return std::lower_bound(vec.begin(), vec.end(), x) - vec.begin();
 	}
-	int size() {
-		return vec.size();
-	}
+	int size() { return vec.size(); }
 	T operator[] (int id) {
 		assert(id < vec.size());
 		return vec[id];
