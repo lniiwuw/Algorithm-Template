4495234b31e4409815039df61babf98f27ab3f6f
lniiwuw <lniiwuw@gmail.com>
2024-04-21 18:32:24 +0800

    最短路dijkstra算法

---
+++ Dijkstra.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+const i64 inf = 0x3f3f3f3f3f3f3f3f;
+
+struct Dijkstra {
+    std::vector<std::vector<std::pair<int, i64>>> adj;
+    int n;
+    
+    Dijkstra(int n, std::vector<std::vector<int>> &d) {
+        this->n = n;
+        adj.resize(n);
+        for (int i = 0; i < d.size(); i++) {
+            auto &k = d[i];
+            int u = d[i][0], v = d[i][1], w = d[i][2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+    }
+    
+    std::vector<i64> work(int s) {
+        std::vector<i64> dis(n, inf);
+        std::vector<bool> vis(n);
+        std::priority_queue<std::pair<i64, int>, std::vector<std::pair<i64, int>>, std::greater<std::pair<i64, int>> > q;
+            
+        dis[s] = 0;
+        q.push({0, s});
+        while (!q.empty()) {
+            auto [d, u] = q.top();
+            q.pop();
+            if (vis[u]) continue;    
+            vis[u] = true;
+            for (auto [v, w] : adj[u]) {
+                if (dis[v] > dis[u] + w) {
+                    dis[v] = dis[u] + w;
+                    q.push({dis[v], v});
+                }
+            }
+        }
+        return dis;
+    }
+};
