6502e5c8a44893e30a331e28a24857a457dca3e1
lniiwuw <lniiwuw@gmail.com>
2024-07-07 15:43:16 +0800

    refactor: 重写哈希函数

--- Hash.cpp
+++ Hash.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 
-using ull = unsigned long long;
 struct stringhash {
-    int n;
-    ull base = 29;
-    std::vector<ull> hash, p;
+    int n, base = 29;
+    int MOD = 1e9 + 7;
+    std::vector<int> hash, p;
  
     stringhash(std::string &text) {
         n = text.size();
@@ -12,22 +11,22 @@ struct stringhash {
         p.resize(n + 1);
         hash[0] = 0, p[0] = 1;
         for(int i = 0; i < n; i++) {
-            hash[i + 1] = hash[i] * base + (text[i] - 'a' + 1);
+            hash[i + 1] = (1LL * hash[i] * base % MOD + text[i]) % MOD;
         }
         for(int i = 1; i <= n;i++) {
-            p[i] = p[i - 1] * base;
+            p[i] = 1LL * p[i - 1] * base % MOD;
         }
     }
     
-    ull RangeQuery (int l, int r) {
-        assert(r <= n);
-        return hash[r + 1] - hash[l] * p[r + 1 - l];
+    int RangeQuery (int l, int r) {
+        assert(r < n);
+        return (hash[r + 1] - 1LL * hash[l] * p[r + 1 - l] % MOD + MOD) % MOD;
     }
 
-    ull getHash (std::string &text) {
-        ull res = 0;
+    int getHash (std::string &text) {
+        int res = 0;
         for (char ch : text) {
-            res = res * base + (ch - 'a' + 1) ;
+            res = (1LL * res * base % MOD + ch) % MOD;
         }
         return res;
     }
