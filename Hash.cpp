5f28fd39fb76123c003b419fca41b33cb7d36339
lniiwuw <lniiwuw@gmail.com>
2024-07-08 15:12:33 +0800

    哈希函数取模 ---> 随机取模

--- Hash.cpp
+++ Hash.cpp
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 
+/**
+多项式字符串哈希（方便计算子串哈希值）
+哈希函数 hash(s) = s[0] * base^(n-1) + s[1] * base^(n-2) + ... + s[n-2] * base + s[n-1]
+pow_base[i] = base^i
+前缀哈希值 pre_hash[i] = hash(s[:i])
+*/
 struct stringhash {
-    int n, base = 29;
-    int MOD = 1e9 + 7;
-    std::vector<int> hash, p;
+    int n, BASE;
+    const int MOD = 1'070'777'777; 
+    std::vector<int> pre_hash, pow_base;
  
     stringhash(std::string &text) {
         n = text.size();
-        hash.resize(n + 1);
-        p.resize(n + 1);
-        hash[0] = 0, p[0] = 1;
-        for(int i = 0; i < n; i++) {
-            hash[i + 1] = (1LL * hash[i] * base % MOD + text[i]) % MOD;
-        }
-        for(int i = 1; i <= n;i++) {
-            p[i] = 1LL * p[i - 1] * base % MOD;
+        std::mt19937 gen{std::random_device{}()};
+        BASE = std::uniform_int_distribution<>(8e8, 9e8)(gen); // 随机 base
+        pow_base.resize(n + 1); 
+        pre_hash.resize(n + 1); // 
+        pow_base[0] = 1;
+        for (int i = 0; i < n; i++) {
+            pow_base[i + 1] = (long long) pow_base[i] * BASE % MOD;
+            pre_hash[i + 1] = ((long long) pre_hash[i] * BASE + text[i]) % MOD;
         }
     }
     
+    // 计算[l, r]的哈希值
     int RangeQuery (int l, int r) {
         assert(r < n);
-        return (hash[r + 1] - 1LL * hash[l] * p[r + 1 - l] % MOD + MOD) % MOD;
+        return ((pre_hash[r + 1] - (long long)pre_hash[l] * pow_base[r + 1 - l]) % MOD + MOD) % MOD;
     }
 
     int getHash (std::string &text) {
         int res = 0;
         for (char ch : text) {
-            res = (1LL * res * base % MOD + ch) % MOD;
+            res = ((long long)res * BASE % MOD + ch) % MOD;
         }
         return res;
     }
